7c69f672e2d9b058d5de48c31cafb413f9573f3d
Stanley Winata <winatastanley355@gmail.com>
2024-11-27 16:50:12 +0700

    Inventories controller and service

--- src/inventories/inventories.module.ts
+++ src/inventories/inventories.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Inventories } from './inventories.entity';
+import { InventoriesService } from './inventories.service';
+import { InventoriesController } from './inventories.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Inventories])],
+  providers: [InventoriesService],
+  controllers: [InventoriesController],
 })
 export class InventoriesModule {}
